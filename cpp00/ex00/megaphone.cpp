5349677fdbce8a2d9ebcf2748011e1365cc8d79a
lalwafi <lyallwafi@gmail.com>
2025-03-17 16:05:13 +0400

    ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lalwafi <lalwafi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 22:53:50 by lalwafi           #+#    #+#             */
+/*   Updated: 2025/03/17 01:29:22 by lalwafi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j] != '\0'; j++)
+			std::cout << (char)toupper(av[i][j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
