b3c499d8c55c672a3dc5aa1a70848c84b89d9e4a
Lyall Alwafi <lalwafi@lab1r4s6.42abudhabi.ae>
2025-07-18 17:03:11 +0400

    cleaned cpp03 more readable

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,17 +6,19 @@
 /*   By: lalwafi <lalwafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 00:34:52 by lalwafi           #+#    #+#             */
-/*   Updated: 2025/07/15 08:34:29 by lalwafi          ###   ########.fr       */
+/*   Updated: 2025/07/18 16:51:16 by lalwafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
 int main(void) {
+	std::cout << "-----------constructors-----------" << std::endl;
     ScavTrap noName;
 	ScavTrap named("kitty");
 	ScavTrap player(named);
     
+	std::cout << "\n----------Attack tests----------" << std::endl;
 	player.attack("enemy");
 	player.attack("enemy");
 	player.attack("enemy");
@@ -24,21 +26,24 @@ int main(void) {
 	player.attack("enemy");
 	player.attack("enemy");
 
+	std::cout << "\n--------Take damage tests--------" << std::endl;
 	player.takeDamage(2);
 	player.takeDamage(0);
 	player.takeDamage(50);
 	player.takeDamage(100);
 	player.takeDamage(2);
 	
+	std::cout << "\n-------Attack tests no hp-------" << std::endl;
+	player.attack("enemy");
 	
+	std::cout << "\n----------repair tests----------" << std::endl;
 	player.beRepaired(0);
 	player.beRepaired(40);
 	
-	player.attack("enemy");
-	player.attack("enemy");
-	player.attack("enemy");
-	player.attack("enemy");
+	std::cout << "\n----Attack tests after repair----" << std::endl;
 	player.attack("enemy");
 	
+	std::cout << "\n-----------Guard gate-----------" << std::endl;
 	player.guardGate();
+	std::cout << "\n-----------destructors-----------" << std::endl;
 }
